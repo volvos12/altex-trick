7ccf7bd87197be39b9f898556c07a9857a260db4
volvos12 <fgetgfet@gmail.com>
2025-03-15 16:28:25 +0200

    Update PriceDisplay.svelte

--- src/lib/components/PriceDisplay.svelte
+++ src/lib/components/PriceDisplay.svelte
@@ -8,10 +8,15 @@
 <div class="flex flex-col max-sm:mt-3">
     <div class="flex gap-2 items-center">
         <span class="text-lg font-mono text-black line-through">1655,93</span>
-        <span class="px-1 py-0 text-sm text-black bg-amber-300 rounded-none">-{discountPercentage}%</span>
+        <span class="px-1 py-[0.5px] text-[0.8rem] rounded-tr-lg rounded-bl-lg font-semibold text-black bg-amber-300 rounded-none">-{discountPercentage}%</span>
     </div>
     <div class="flex gap-1 items-center text-4xl font-bold text-rose-700">
         <span>{currentPrice}</span>
-        <span class="text-3xl font-bold text-rose-700">{currency}</span>
+        <div>
+            <div class="relative w-[5vw] h-full">
+                <span class="text-2xl font-bold text-rose-700 top-0 absolute left-0">,91</span>
+            </div>
+            <span class="text-3xl font-bold ml-[2.5rem] text-rose-700">{currency}</span>
+        </div>
     </div>
 </div>
